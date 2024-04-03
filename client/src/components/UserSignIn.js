db221a075619126701e713afe23f3ce19009e31b
Seth Foss <thelizardreborn@live.com>
2024-04-03 13:10:57 -0400

    Move signin logic and user data to user context

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,9 +1,11 @@
 import { useContext, useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import ThemeContext from '../context/ThemeContext';
+import UserContext from '../context/UserContext';
 
 const UserSignIn = () => {
   const { accentColor } = useContext(ThemeContext);
+  const { actions } = useContext(UserContext);
   const navigate = useNavigate();
 
   // State
@@ -18,27 +20,14 @@ const UserSignIn = () => {
     const credentials = {
       username: username.current.value,
       password: password.current.value,
-    }
-
-    const encodedCredentials = btoa(`${credentials.username}:${credentials.password}`);
-
-    const fetchOptions = {
-      method: "GET",
-      headers: {
-        Authorization: `Basic ${encodedCredentials}`,
-      },
-    }
+    };
 
     try {
-      const response = await fetch("http://localhost:5000/api/users", fetchOptions);
-      if (response.status === 200) {
-        const user = await response.json();
-        console.log(`Success! ${user.username} is now signed in!`);
+      const user = await actions.signIn(credentials);
+      if (user) {
         navigate("/authenticated");
-      } else if (response.status === 401) {
-        const data = await response.json();
-        console.log(data);
-        setErrors(["Sign in was unsuccessful!"]);
+      } else {
+        setErrors(["Sign in was unsuccessful"]);
       }
     } catch (error) {
       console.log(error);
@@ -65,7 +54,7 @@ const UserSignIn = () => {
                 </ul>
               </div>
             </div>
-          ) : null }
+          ) : null}
           <form onSubmit={handleSubmit}>
             <input
               id="username"
--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,11 +1,30 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const UserContext = createContext(null);
 
 export const UserProvider = (props) => {
+  const [authUser, setAuthUser] = useState(null);
 
-  const signIn = async () => {
+  const signIn = async (credentials) => {
+    const encodedCredentials = btoa(`${credentials.username}:${credentials.password}`);
 
+    const fetchOptions = {
+      method: "GET",
+      headers: {
+        Authorization: `Basic ${encodedCredentials}`,
+      },
+    };
+
+    const response = await fetch("http://localhost:5000/api/users", fetchOptions);
+    if (response.status === 200) {
+      const user = await response.json();
+      setAuthUser(user);
+      return user;
+    } else if (response.status === 401) {
+      return null;
+    } else {
+      throw new Error();
+    }
   }
 
   const signOut = () => {
@@ -13,7 +32,12 @@ export const UserProvider = (props) => {
   }
 
   return (
-    <UserContext.Provider value={{}}>
+    <UserContext.Provider value={{
+      authUser,
+      actions: {
+        signIn,
+      }
+    }}>
       {props.children}
     </UserContext.Provider>
   );
