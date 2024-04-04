24b142336d39703a9739cbf03ca835cc9377951f
Seth Foss <thelizardreborn@live.com>
2024-04-03 21:17:33 -0400

    Store theme state in cookie

--- client/src/context/ThemeContext.js
+++ client/src/context/ThemeContext.js
@@ -1,11 +1,18 @@
+import Cookies from "js-cookie";
 import { createContext, useState, useEffect } from "react";
 
 const ThemeContext = createContext(null);
 
 export const ThemeProvider = (props) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [accentColor, setAccentColor] = useState('#63537d');
-  const [fontPercentage, setFontPercentage] = useState(100);
+  const cookie = Cookies.get("defaultTheme");
+  const defaultTheme = cookie ? JSON.parse(cookie) : {
+    isDarkMode: false,
+    accentColor: '#63537d',
+    fontPercentage: 100
+  }
+  const [isDarkMode, setIsDarkMode] = useState(defaultTheme.isDarkMode);
+  const [accentColor, setAccentColor] = useState(defaultTheme.accentColor);
+  const [fontPercentage, setFontPercentage] = useState(defaultTheme.fontPercentage);
 
   useEffect(() => {
     if (isDarkMode) {
@@ -14,7 +21,14 @@ export const ThemeProvider = (props) => {
       document.body.classList.remove('dark');
     }
     document.body.style.fontSize = `${fontPercentage}%`;
-  }, [isDarkMode, fontPercentage]);
+
+    const theme = {
+      isDarkMode,
+      accentColor,
+      fontPercentage,
+    };
+    Cookies.set("defaultTheme", JSON.stringify(theme));
+  }, [isDarkMode, fontPercentage, accentColor]);
 
   const toggleDarkMode = () => {
     setIsDarkMode(currentMode => !currentMode);
