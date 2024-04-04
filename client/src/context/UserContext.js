04e7a11e12ccde4d6407ef94b3ee76156c3c306c
Seth Foss <thelizardreborn@live.com>
2024-04-03 21:13:17 -0400

    Store user data in Cookie to persist across page refreshes

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,9 +1,11 @@
 import { createContext, useState } from "react";
+import Cookies from "js-cookie";
 
 const UserContext = createContext(null);
 
 export const UserProvider = (props) => {
-  const [authUser, setAuthUser] = useState(null);
+  const cookie = Cookies.get("authenticatedUser");
+  const [authUser, setAuthUser] = useState(cookie ? JSON.parse(cookie) : null);
 
   const signIn = async (credentials) => {
     const encodedCredentials = btoa(`${credentials.username}:${credentials.password}`);
@@ -19,6 +21,7 @@ export const UserProvider = (props) => {
     if (response.status === 200) {
       const user = await response.json();
       setAuthUser(user);
+      Cookies.set("authenticatedUser", JSON.stringify(user), {expires: 1});
       return user;
     } else if (response.status === 401) {
       return null;
@@ -28,6 +31,7 @@ export const UserProvider = (props) => {
   }
 
   const signOut = () => {
+    Cookies.remove("authenticatedUser");
     setAuthUser(null);
   }
 
