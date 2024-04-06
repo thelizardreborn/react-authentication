508853778d53cf9527c5a766bd7ee4895a28137c
Seth Foss <thelizardreborn@live.com>
2024-04-06 14:12:03 -0400

    Add test theme change

--- client/src/__tests__/Settings.test.js
+++ client/src/__tests__/Settings.test.js
@@ -15,3 +15,23 @@ test("Settings renders successfully", () => {
    const darkModeSwitch = screen.getByRole("checkbox", {className: "darkMode-selector"});
    expect(darkModeSwitch).toBeInTheDocument();
 });
+
+test("Settings callbacks are called", () => {
+   const defaultTheme = {
+      isDarkMode: false,
+      accentColor: '#63537d',
+      fontPercentage: 100,
+   }
+   Cookie.get = jest.fn().mockImplementation(() => JSON.stringify(defaultTheme));
+
+   render(
+      <ThemeProvider>
+         <Settings />
+      </ThemeProvider>
+   );
+
+   expect(document.body).not.toHaveClass('dark');
+   const darkModeSwitch = screen.getByRole("checkbox", {className: "darkMode-selector"});
+   fireEvent.click(darkModeSwitch);
+   expect(document.body).toHaveClass('dark');
+});
